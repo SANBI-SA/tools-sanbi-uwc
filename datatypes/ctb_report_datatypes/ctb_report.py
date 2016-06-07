4acac74a669ec2a4c6b4d4c1c3c227ab61ebbed7
zipho <zipho.m@gmail.com>
2016-06-07 16:11:12 +0200

    changes

--- datatypes/ctb_report_datatypes/ctb_report.py
+++ datatypes/ctb_report_datatypes/ctb_report.py
@@ -5,7 +5,7 @@ import logging
 import sys
 
 from galaxy.datatypes.data import Data
-from galaxy.datatypes.images import Html
+#from galaxy.datatypes.images import Html
 
 gal_Log = logging.getLogger(__name__)
 verbose = True
@@ -63,89 +63,13 @@ class CtbReport(object):
         return "<html><head><title>%s</title></head><body><pre>%s</pre></body></html>" % (title, msg)
 
 
-class CtbReportsNeo4j(CtbReport, Data):
-    """Class for neo4jDB database files."""
-    file_ext = 'ctbreportneo4j'
+class CtbReportSet(CtbReport, Data):
+    """Class for Ctb Report Set files."""
+    file_ext = 'ctbreport'
     composite_type = 'basic'
     allow_datatype_change = False
 
     def __init__(self, **kwd):
         Data.__init__(self, **kwd)
-        self.add_composite_file('neostore', substitute_name_with_metadata='neostore', is_binary=True)
-        self.add_composite_file('neostore.id', substitute_name_with_metadata='neostore', is_binary=True)
-        self.add_composite_file('neostore.counts.db.a', substitute_name_with_metadata='neostore_count_file',
-                                is_binary=True)
-        self.add_composite_file('neostore.counts.db.b', substitute_name_with_metadata='neostore_count_file',
-                                is_binary=True)
-        self.add_composite_file('neostore.labeltokenstore.db',
-                                substitute_name_with_metadata='neostore_labeltokenstore_db_file', is_binary=True)
-        self.add_composite_file('neostore.labeltokenstore.db.id',
-                                substitute_name_with_metadata='neostore_labeltokenstore_db_file', is_binary=True)
-        self.add_composite_file('neostore.labeltokenstore.db.names',
-                                substitute_name_with_metadata='neostore_labeltokenstore_db_file', is_binary=True)
-        self.add_composite_file('neostore.labeltokenstore.db.names.id',
-                                substitute_name_with_metadata='neostore_labeltokenstore_db_file', is_binary=True)
-        self.add_composite_file('neostore.nodestore.db', substitute_name_with_metadata='neostore_nodestore_file',
-                                is_binary=True)
-        self.add_composite_file('neostore.nodestore.db.id', substitute_name_with_metadata='neostore_nodestore_file',
-                                is_binary=True)
-        self.add_composite_file('neostore.nodestore.db.labels', substitute_name_with_metadata='neostore_nodestore_file',
-                                is_binary=True)
-        self.add_composite_file('neostore.nodestore.db.labels.id',
-                                substitute_name_with_metadata='neostore_nodestore_file', is_binary=True)
-
-        self.add_composite_file('neostore.propertystore.db',
-                                substitute_name_with_metadata='neostore_propertystore_file', is_binary=True)
-        self.add_composite_file('neostore.propertystore.db.id',
-                                substitute_name_with_metadata='neostore_propertystore_file', is_binary=True)
-        self.add_composite_file('neostore.propertystore.db.arrays',
-                                substitute_name_with_metadata='neostore_propertystore_file', is_binary=True)
-        self.add_composite_file('neostore.propertystore.db.arrays.id',
-                                substitute_name_with_metadata='neostore_propertystore_file', is_binary=True)
-        self.add_composite_file('neostore.propertystore.db.index',
-                                substitute_name_with_metadata='neostore_propertystore_file', is_binary=True)
-        self.add_composite_file('neostore.propertystore.db.index.id',
-                                substitute_name_with_metadata='neostore_propertystore_file', is_binary=True)
-        self.add_composite_file('neostore.propertystore.db.index.keys',
-                                substitute_name_with_metadata='neostore_propertystore_file', is_binary=True)
-        self.add_composite_file('neostore.propertystore.db.index.keys.id',
-                                substitute_name_with_metadata='neostore_propertystore_file', is_binary=True)
-        self.add_composite_file('neostore.propertystore.db.strings',
-                                substitute_name_with_metadata='neostore_propertystore_file', is_binary=True)
-        self.add_composite_file('neostore.propertystore.db.strings.id',
-                                substitute_name_with_metadata='neostore_propertystore_file', is_binary=True)
-
-        self.add_composite_file('neostore.relationshipgroupstore.db',
-                                substitute_name_with_metadata='neostore_relationship_group_file', is_binary=True)
-        self.add_composite_file('neostore.relationshipgroupstore.db.id',
-                                substitute_name_with_metadata='neostore_relationship_group_file', is_binary=True)
-        self.add_composite_file('neostore.relationshipstore.db',
-                                substitute_name_with_metadata='neostore_relationship_file', is_binary=True)
-        self.add_composite_file('neostore.relationshipstore.db.id',
-                                substitute_name_with_metadata='neostore_relationship_file', is_binary=True)
-        self.add_composite_file('neostore.relationshiptypestore.db.names',
-                                substitute_name_with_metadata='neostore_relationship_type_file', is_binary=True)
-        self.add_composite_file('neostore.relationshiptypestore.db.names.id',
-                                substitute_name_with_metadata='neostore_relationship_type_file', is_binary=True)
-        self.add_composite_file('neostore.schemastore.db', substitute_name_with_metadata='neostore_schema_store_file',
-                                is_binary=True)
-        self.add_composite_file('neostore.schemastore.db.id',
-                                substitute_name_with_metadata='neostore_schema_store_file', is_binary=True)
-        self.add_composite_file('neostore.transaction.db.0', substitute_name_with_metadata='neostore_count_file',
-                                is_binary=True)
-
-
-class CtbReportsJBrowse(CtbReport, Html):
-    """Class for ctb report jbrowse files."""
-    file_ext = 'ctbreportjbrowse'
-    composite_type = 'basic'
-    allow_datatype_change = False
-
-    def __init__(self, **kwd):
-        Html.__init__(self, **kwd)
-
-
-if __name__ == '__main__':
-    import doctest
-
-    doctest.testmod(sys.modules[__name__])
+        self.add_composite_file('neo4j/',  is_binary=True)
+        self.add_composite_file('jbrowser/', is_binary=True)
